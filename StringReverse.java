bb6f3d1178564726839f7970a340311dc98ce756
aruthmari <96566448+aruthmari@users.noreply.github.com>
2022-07-09 18:48:59 +0530

    Add files via upload

---
+++ StringReverse.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringReverse {
+	
+	static String reverse(String sentence) {
+		String[] words = sentence.split(" ");
+		String revString = "";
+		for (String word : words) {
+			StringBuilder abc = new StringBuilder(word);
+			revString = revString+ abc.reverse()+" ";
+		}return revString;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the sentence : ");
+		String sentence = sc.nextLine();
+		
+		String revSentence = reverse(sentence);
+		System.out.println(sentence);
+		System.out.println(revSentence);
+	}
+
+}
